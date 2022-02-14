88ac3cfcdbdf7facdecbb780948f182e9c5b912a
bertilio <alberto.vicentee@edu.uah.es>
2022-02-14 01:04:08 +0100

    u

--- entrenar.py
+++ entrenar.py
@@ -20,12 +20,7 @@ def entrenar(n):
         a2.actualizar(pasos,ganador)
 
 
-        print("-------------------")
-        print("partida: " + str(a.iteracion))
-        print("-------------------")
-        print("estados: " + str(len(a.estados)))
-        print("-------------------")
-        print("|||||||||||||||||||")
+
 
         if (a.iteracion == 10) or (a.iteracion == 100) or (a.iteracion == 1000) or (a.iteracion == 10000) or (a.iteracion == 50000) or (a.iteracion == 500000) or (a.iteracion == n):
             string = "agenteX"
